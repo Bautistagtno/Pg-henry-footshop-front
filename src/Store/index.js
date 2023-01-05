df52593fee802361955d6f0455047f1131df0fb1
GastonFlorenciano <gaston__florenciano@hotmail.com>
2023-01-05 18:15:18 -0300

    store creada

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,9 +2,5 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../Reducer/index';
-'hola'
-'hola'
-'hola'
-'hola'
-'hola'
+
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))

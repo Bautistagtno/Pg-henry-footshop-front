357b4a1b47d520dbd590dd6c1bf7b8c241452413
ale2trelew <ale2trelew@gmail.com>
2023-01-08 10:04:31 -0300

    agregados los demas talles

--- src/Components/Filters/Filters.jsx
+++ src/Components/Filters/Filters.jsx
@@ -44,9 +44,24 @@ export const Filters = () => {
             </select>
             <label>Selecciona una talla</label>
             <select onClick={(e) => filterTalla(e)}>
+                <option value={34}> 34 </option>
+                <option value={35}> 35 </option>
+                <option value={36}> 36 </option>
+                <option value={37}> 37 </option>
+                <option value={37.5}> 37.5 </option>
                 <option value={38}> 38 </option>
                 <option value={39}> 39 </option>
                 <option value={40}> 40 </option>
+                <option value={41}> 41 </option>
+                <option value={41.5}> 41.5 </option>
+                <option value={42}> 42 </option>
+                <option value={43}> 43 </option>
+                <option value={44}> 44 </option>
+                <option value={45}> 45 </option>
+                <option value={46}> 46 </option>
+                <option value={47}> 47 </option>
+                <option value={48}> 48 </option>
+                <option value={49}> 49 </option>
             </select>
             <button onClick={() => handleOnFilters()}> Aplicar filtros </button>
         </div>

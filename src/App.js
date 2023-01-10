2d7de0b777661adaa60ddd56e69e1f7c36754683
Your Name <you@example.com>
2023-01-10 19:04:44 -0300

    commit autenticacion

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Home from './Components/Home';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Details from './Components/Detail';
 import Formulario from './Components/Formulario/Formulario'
+import Login from './Components/Login/Login';
+
 
 function App() {
   return (
     <BrowserRouter>
     <div>
+      <Login/>
       <Routes>
+        <Route exact path='/login' element={<Login/>}/>
         <Route exact path = '/' element={<LandingPage/>} />
         <Route path = '/Home' element={<Home/>} />
         <Route exact path = '/crear' element={<Formulario/>} />

f6d4a60fdf7afaacaeaa8f02006b536204fdfc4c
sayan990 <maximilianocaceres1601@gmail.com>
2023-01-13 17:09:29 -0300

    modified:   src/App.js 	new file:   src/Components/Admin/uploadImg.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Components/Home';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Details from './Components/Detail';
 import Formulario from './Components/Formulario/Formulario'
 import Login from './Components/Login/Login';
+import UploadImg from "./Components/Admin/uploadImg";
 
 
 function App() {
@@ -16,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/login' element={<Login/>}/>
         <Route exact path = '/' element={<LandingPage/>} />
+        <Route exact path='/uploadImg' element={<UploadImg/>}/>
         <Route path = '/Home' element={<Home/>} />
         <Route exact path = '/crear' element={<Formulario/>} />
         <Route exact path='/zapatillas/:id' element={<Details/>}/>
